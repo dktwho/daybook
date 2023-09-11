8900d38539c3c1287391072fe2fdb97256ac4088
Alexander Korobov <dktwho@mail.ru>
2023-09-11 15:28:10 +0300

    add some value for jsx

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,12 +1,17 @@
 
 export const JournalItem = () => {
+    const title  = 'Подготовка к обновлению курсов';
+    const date = new Date()
+    const text = 'Горные походы открывают удивительные природные ландшафты.'
+
     return (
         <div className={'journal-item'}>
-            <h2 className={'journal-item__header'}>1</h2>
+            <h2 className={'journal-item__header'}>{title}</h2>
             <h2 className={'journal-item__body'}>
-                <div className={'journal-item__date'}>2</div>
-                <div className={'journal-item__text'}>3</div>
+                <div className={'journal-item__date'}>{date.toString()}</div>
+                <div className={'journal-item__text'}>{text}</div>
             </h2>
         </div>
     );
 };
+
