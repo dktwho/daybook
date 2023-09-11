ff45d1deda84c29a3efd86a8a5c057bda4ec9ceb
Alexander Korobov <dktwho@mail.ru>
2023-09-11 15:21:53 +0300

    create component JournalItem and added style

---
+++ src/components/JournalItem/JournalItem.jsx
@@ -0,0 +1,12 @@
+
+export const JournalItem = () => {
+    return (
+        <div className={'journal-item'}>
+            <h2 className={'journal-item__header'}>1</h2>
+            <h2 className={'journal-item__body'}>
+                <div className={'journal-item__date'}>2</div>
+                <div className={'journal-item__text'}>3</div>
+            </h2>
+        </div>
+    );
+};
