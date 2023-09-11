f7a527fe3f430d555ce7ad41e152226623a4614f
Alexander Korobov <dktwho@mail.ru>
2023-09-11 15:44:59 +0300

    new Intl.DateTimeFormat

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,11 +1,14 @@
 import './JournalItem.css'
+
 export const JournalItem = (props) => {
     const {title, date, text} = props
+    const formatedDate = new Intl.DateTimeFormat('ru-Ru').format(date)
+
     return (
         <div className={'journal-item'}>
             <h2 className={'journal-item__header'}>{title}</h2>
             <h2 className={'journal-item__body'}>
-                <div className={'journal-item__date'}>{date.toString()}</div>
+                <div className={'journal-item__date'}>{formatedDate}</div>
                 <div className={'journal-item__text'}>{text}</div>
             </h2>
         </div>
