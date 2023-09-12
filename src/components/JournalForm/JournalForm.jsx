26813e1662d5065fa777e5954be6b6698ff17103
Alexander Korobov <dktwho@mail.ru>
2023-09-12 16:17:40 +0300

    check value from form, validate

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,13 +1,38 @@
 import {Button} from "../Button/Button.jsx";
+import {useState} from "react";
 
 export const JournalForm = ({addNewPost}) => {
+    const [formValidState, setFormValidState] = useState({
+        id: true,
+        title: true,
+        text: true,
+        date: true,
+        tag: true
+    })
 
     const addJournalItem = (e) => {
         e.preventDefault()
         const formData = new FormData(e.target)
         const formProps = Object.fromEntries(formData)
+        let isFormValid = true
+        if (!formProps.title?.trim().length) {
+            setFormValidState(state => ({...state, title: false}))
+            isFormValid = false
+        }
+        if (!formProps.text?.trim().length) {
+            setFormValidState(state => ({...state, text: false}))
+            isFormValid = false
+        }
+        if (!formProps.date) {
+            setFormValidState(state => ({...state, date: false}))
+            isFormValid = false
+        }
+        if (!isFormValid) {
+            return
+        }
         addNewPost(formProps)
     }
+
 
     return (
         <form className={'journal-form'} onSubmit={addJournalItem}>
@@ -15,7 +40,7 @@ export const JournalForm = ({addNewPost}) => {
             <input type="date" name={'date'}/>
             <input name={'tag'} type="text"/>
             <textarea name="text" id="" cols="30" rows="10"></textarea>
-            <Button text={'Submit'} onClick={() => console.log('clicked')}/>
+            <Button text={'Submit'}/>
         </form>
     );
 };
