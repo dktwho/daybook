2d59a3322ff0578e177e5bcd082e097ebdac7e47
Alexander Korobov <dktwho@mail.ru>
2023-09-12 11:51:24 +0300

    add conditional render , if arr data length === 0

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,41 @@ import {Body} from "../layout/Body/Body.jsx";
 import {JournalAddButton} from "./components/JournalAddButton/JournalAddButton.jsx";
 import {JournalForm} from "./components/JournalForm/JournalForm.jsx";
 import {useState} from "react";
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
     const data = [
-        {
-            id: uuidv4(),
-            title: 'Подготовка к обновлению курсов',
-            text: 'Горные походы открывают удивительные природные ландшафты',
-            date: new Date(),
-            tag: 'tag1'
-
-        },
-        {
-            id: uuidv4(),
-            title: 'Поход в горы',
-            text: 'Различают альпинизм и горный туризм',
-            date: new Date(),
-            tag: 'tag2'
-        }
+        // {
+        //     id: uuidv4(),
+        //     title: 'Подготовка к обновлению курсов',
+        //     text: 'Горные походы открывают удивительные природные ландшафты',
+        //     date: new Date(),
+        //     tag: 'tag1'
+        //
+        // },
+        // {
+        //     id: uuidv4(),
+        //     title: 'Поход в горы',
+        //     text: 'Различают альпинизм и горный туризм',
+        //     date: new Date(),
+        //     tag: 'tag2'
+        // }
     ];
 
     const [items, setItems] = useState(data)
     const addNewItem = (item) => {
-        let newItem =  {
+        let newItem = {
             id: uuidv4(),
             text: item.text,
             title: item.title,
             tag: item.tag,
             date: new Date(item.date)
         }
-        setItems(oldItems => ([...oldItems, newItem ]))
+        setItems(oldItems => ([...oldItems, newItem]))
 
     }
 
-    const sortItems = (a,b) => a.date < b.date ? 1 : -1
+    const sortItems = (a, b) => a.date < b.date ? 1 : -1
 
     return (
         <div className={'app'}>
@@ -50,7 +50,8 @@ function App() {
                 <Header/>
                 <JournalAddButton/>
                 <JournalList>
-                    {items.sort(sortItems).map(p => {
+                    {!items.length === 0 && <p>add you first story</p>}
+                    {items.length > 0 && items.sort(sortItems).map(p => {
                         return (
                             <CardButton key={p.id}>
                                 <JournalItem title={p.title} date={p.date} text={p.text}/>
