3aa3f22717cb3ad1700a7fbdf6ba63438f96ceff
Alexander Korobov <dktwho@mail.ru>
2023-09-12 17:17:08 +0300

    modified styles with classnames, fixed styles for form

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -42,14 +42,31 @@ export const JournalForm = ({addNewPost}) => {
     }
 
 
-
     return (
         <form className={s.journalForm} onSubmit={addJournalItem}>
-            <input type="text" name={'title'} className={cn(s['input'], {[s['invalid']] : !formValidState.title })}/>
-            <input type="date" name={'date'}  className={cn(s['input'], {[s['invalid']] : !formValidState.date })}/>
-            <input name={'tag'} type="text"/>
+            <div>
+                <input type="text" name={'title'}
+                       className={cn(s['input-title'], {[s['invalid']]: !formValidState.title})}/>
+            </div>
+
+            <div className={s['form-row']}>
+                <label htmlFor="date" className={s['form-label']}>
+                    <img src="/Frame1.svg" alt="icon-calendar"/>
+                    <span>Date</span></label>
+                <input type="date" id={'date'} name={'date'}
+                       className={cn(s['input'], {[s['invalid']]: !formValidState.date})}/>
+            </div>
+
+            <div className={s['form-row']}>
+                <label htmlFor="tag" className={s['form-label']}>
+                    <img src="/Frame2.svg" alt="icon-tag"/>
+                    <span>Tag</span></label>
+                <input name={'tag'} id={'tag'} type="text"
+                       className={cn(s['input'], {[s['invalid']]: !formValidState.text})}/>
+            </div>
+
             <textarea name="text" id="" cols="30" rows="10"
-                      className={cn(s['input'], {[s['invalid']] : !formValidState.text })}/>
+                      className={cn(s['input'], {[s['invalid']]: !formValidState.text})}/>
             <Button text={'Submit'}/>
         </form>
     );
