44e5aaf7112d78c5fbdc1bbcb5aa6a61c147b132
Alexander Korobov <dktwho@mail.ru>
2023-09-13 17:02:33 +0300

    add useEffect, with empty array in dependencies, inside useEffect gwtData from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,20 @@ import {JournalList} from "./components/JournalList/JournalList.jsx";
 import {Body} from "../layout/Body/Body.jsx";
 import {JournalAddButton} from "./components/JournalAddButton/JournalAddButton.jsx";
 import {JournalForm} from "./components/JournalForm/JournalForm.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {v4 as uuidv4} from 'uuid';
 
 function App() {
-    const data = [
-        // {
-        //     id: uuidv4(),
-        //     title: 'Подготовка к обновлению курсов',
-        //     text: 'Горные походы открывают удивительные природные ландшафты',
-        //     date: new Date(),
-        //     tag: 'tag1'
-        //
-        // },
-        // {
-        //     id: uuidv4(),
-        //     title: 'Поход в горы',
-        //     text: 'Различают альпинизм и горный туризм',
-        //     date: new Date(),
-        //     tag: 'tag2'
-        // }
-    ];
+    const [items, setItems] = useState([])
 
-    const [items, setItems] = useState(data)
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem('data'))
+        if (data) {
+            setItems(data.map(el => ({
+                ...el, date: new Date(el.date)
+            })))
+        }
+    }, [])
     const addNewItem = (item) => {
         let newItem = {
             id: uuidv4(),
@@ -56,3 +47,4 @@ function App() {
 }
 
 export default App
+
