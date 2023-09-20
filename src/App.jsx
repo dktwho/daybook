7f50c7aca22c16d2a3b448ef865c2001118d43b0
Alexander Korobov <dktwho@mail.ru>
2023-09-20 16:18:47 +0300

    create custom hook useLocalStorageHook, where is logic for getItem from localStorage and setItem

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,26 @@ import {JournalList} from "./components/JournalList/JournalList.jsx";
 import {Body} from "../layout/Body/Body.jsx";
 import {JournalAddButton} from "./components/JournalAddButton/JournalAddButton.jsx";
 import {JournalForm} from "./components/JournalForm/JournalForm.jsx";
-import {useEffect, useState} from "react";
 import {v4 as uuidv4} from 'uuid';
+import {useLocalStorage} from "./hooks/useLocalStorageHook.js";
+
+const mapItems = (items) => {
+    if (!items) {
+        return []
+    }
+    return items.map(i => ({...i, date: new Date(i.date)}))
+}
 
 function App() {
-    const [items, setItems] = useState([])
-
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem('data'))
-        if (data) {
-            setItems(data.map(el => ({
-                ...el, date: new Date(el.date)
-            })))
-        }
-    }, [])
-
-    useEffect(() => {
-        if(items.length) {
-            localStorage.setItem('data', JSON.stringify(items))
-        }
-    }, [items])
+    const [items, setItems] = useLocalStorage('data')
     const addNewItem = (item) => {
-        let newItem = {
+        setItems([...mapItems(items), {
             id: uuidv4(),
             text: item.text,
             title: item.title,
             tag: item.tag,
             date: new Date(item.date)
-        }
-        setItems(oldItems => ([...oldItems, newItem]))
-
+        }])
     }
 
 
@@ -43,7 +33,7 @@ function App() {
             <LeftPanel>
                 <Header/>
                 <JournalAddButton/>
-                <JournalList items={items}/>
+                <JournalList items={mapItems(items)}/>
             </LeftPanel>
             <Body>
                 <JournalForm addNewPost={addNewItem}/>
