8833a7915533007622ba484b486707f26cd30d3b
Alexander Korobov <dktwho@mail.ru>
2023-12-01 18:12:56 +0300

    feat: modified Header, comment img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import {SelectUser} from "../SelectUser/SelectUser.jsx";
 export const Header = () => {
     return (
         <>
-            <img className={s.logo} src="/logo.svg" alt="logo"/>
+            {/*<img className={s.logo} src="/logo.svg" alt="logo"/>*/}
             <SelectUser/>
         </>
     );
