9362bc93388f2a32f1e167772fb39f91b7794a33
Alexander Korobov <dktwho@mail.ru>
2023-12-04 19:56:36 +0300

    feat:add button inside header, toggle logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import {SelectUser} from "../SelectUser/SelectUser.jsx";
+import {useState} from "react";
+import s from './Header.module.css'
+import {Button} from "../Button/Button.jsx";
+
+const logos = ['/logo.svg', './vite.svg']
 
 export const Header = () => {
+    const [logoIndex, setLogoIndex] = useState(0)
+    const toggleLogo = () => {
+        setLogoIndex(prev => Number(!prev))
+    }
     return (
         <>
-            {/*<img className={s.logo} src="/logo.svg" alt="logo"/>*/}
+            <img className={s.logo} src={logos[logoIndex]} alt="logo"/>
             <SelectUser/>
+            <Button onClick={toggleLogo} text={'Change logo'}/>
         </>
     );
 };
