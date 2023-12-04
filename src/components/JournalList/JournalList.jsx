ecf8bef1551a4f782d5dfb358f5f7741c9c947ab
Alexander Korobov <dktwho@mail.ru>
2023-12-04 19:32:02 +0300

    feat:add user context in Journal list

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,8 +1,11 @@
 import './JournalList.css'
 import {CardButton} from "../CardButton/CardButton.jsx";
 import {JournalItem} from "../JournalItem/JournalItem.jsx";
+import {useContext} from "react";
+import {UserContext} from "../../context/user.context.jsx";
 
 export const JournalList = ({items}) => {
+    const {userId} = useContext(UserContext)
     if (items.length === 0) {
         return <p>add you first story</p>
     }
@@ -10,13 +13,16 @@ export const JournalList = ({items}) => {
 
     return (
         <>
-            {items.sort(sortItems).map(p => {
-                return (
-                    <CardButton key={p.id}>
-                        <JournalItem title={p.title} date={p.date} text={p.text} tag={p.tag}/>
-                    </CardButton>
-                )
-            })}
+            {items
+                .filter(el => el.userId === userId)
+                .sort(sortItems)
+                .map(p => {
+                    return (
+                        <CardButton key={p.id}>
+                            <JournalItem title={p.title} date={p.date} text={p.text} tag={p.tag}/>
+                        </CardButton>
+                    )
+                })}
         </>
     );
 };
